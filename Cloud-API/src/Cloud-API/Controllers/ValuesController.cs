a863532e1b42daebe787f7c2a75f5da90591ee42
Xavi Garcia <antiwindows17@gmail.com>
2016-10-31 09:58:16 +0100

    Models

--- Cloud-API/src/Cloud-API/Controllers/ValuesController.cs
+++ Cloud-API/src/Cloud-API/Controllers/ValuesController.cs
@@ -4,41 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Cloud_API.Controllers
-{
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
-    {
+namespace Cloud_API.Controllers {
+    [Route("api/values")]
+    public class ValuesController : Controller {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
+        public IEnumerable<string> Get() {
+            return new[] {"Hello", "world"};
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
+        public string Get(int id) {
+            return (id*2).ToString();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
-        {
+        public IActionResult Post([FromBody] string value) {
+            return NoContent();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        public void Put(int id, [FromBody] string value) {}
+
+        // PATCH api/values/5
+        [HttpPatch("{id}")]
+        public void Patch(int id, [FromBody] string value) {}
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        public void Delete(int id) {}
     }
 }
---
+++ Cloud-API/src/Cloud-API/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloud_API.Models
+{
+    public class Item
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+    }
+}
