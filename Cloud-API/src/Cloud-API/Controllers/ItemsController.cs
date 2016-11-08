1a4607b28fd090ed3413a97e5728332348787a0a
Xavi Garcia <antiwindows17@gmail.com>
2016-11-08 11:24:47 +0100

    new database table

--- Cloud-API/src/Cloud-API/Controllers/ItemsController.cs
+++ Cloud-API/src/Cloud-API/Controllers/ItemsController.cs
@@ -23,7 +23,7 @@ namespace Cloud_API.Controllers {
         //GET /api/items/id
         [HttpGet("{id}", Name = "GetItem")]
         public IActionResult Get(int id) {
-            var res = _context.Items.FirstOrDefault(i => i.Key == id);
+            var res = _context.Items.FirstOrDefault(i => i.ID == id);
             //var res = _context.Items.ToList().Find(i => i.Key == id);
             if (res == default(Item)) {
                 return NotFound();
@@ -37,13 +37,14 @@ namespace Cloud_API.Controllers {
             if (item == null) return BadRequest();
             if (ModelState.IsValid) {
                 try {
+                    item.Data = DateTime.Now;
                     _context.Items.Add(item);
                     _context.SaveChanges();
                 } catch (Exception ex) {
                     Debug.WriteLine(ex.Message);
                     return BadRequest();
                 }
-                return CreatedAtRoute("GetItem", new { id = item.Key }, item);
+                return CreatedAtRoute("GetItem", new { id = item.ID }, item);
             }
 
             return NoContent();
@@ -51,13 +52,13 @@ namespace Cloud_API.Controllers {
 
         // PUT api/items/id
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]Item value) {
-            if (value == null) return BadRequest();
-            var item = _context.Items.FirstOrDefault(i => i.Key == id);
+        public IActionResult Put(int id, [FromBody]Item nouItem) {
+            if (nouItem == null) return BadRequest();
+            var item = _context.Items.FirstOrDefault(i => i.ID == id);
             if (item == default(Item)) return NotFound();
             if (ModelState.IsValid) {
                 try {
-                    item = value;
+                    item.Update(nouItem);
                     _context.Items.Update(item);
                     _context.SaveChanges();
                 } catch (Exception ex) {
@@ -71,13 +72,16 @@ namespace Cloud_API.Controllers {
 
         // PATCH api/items/id
         [HttpPatch("{id}")]
-        public IActionResult Patch(int id, [FromBody]Item value) {
-            if (value == null) return BadRequest();
-            var item = _context.Items.FirstOrDefault(i => i.Key == id);
+        public IActionResult Patch(int id, [FromBody]Item nouItem) {
+            if (nouItem == null) return BadRequest();
+            var item = _context.Items.FirstOrDefault(i => i.ID == id);
             if (item == default(Item)) return NotFound();
             if (ModelState.IsValid) {
                 try {
-                    item.Name = value.Name; //todo per a Item mes complex metode aux
+                    if (nouItem.Equip != null) item.Equip = nouItem.Equip;
+                    if (nouItem.Value != 0) item.Value = nouItem.Value;
+                    if (nouItem.Aux != null) item.Aux = nouItem.Aux;
+                    item.Data = DateTime.Now;
                     _context.Items.Update(item);
                     _context.SaveChanges();
                 } catch (Exception ex) {
@@ -92,7 +96,7 @@ namespace Cloud_API.Controllers {
         // DELETE api/items/id
         [HttpDelete("{id}")]
         public IActionResult Delete(int id) {
-            var item = _context.Items.FirstOrDefault(i => i.Key == id);
+            var item = _context.Items.FirstOrDefault(i => i.ID == id);
             if (item == default(Item)) return NotFound();
             if (ModelState.IsValid) {
                 _context.Items.Remove(item);
--- Cloud-API/src/Cloud-API/Models/Item.cs
+++ Cloud-API/src/Cloud-API/Models/Item.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Cloud_API.Models {
     public class ItemContext : DbContext {
@@ -6,14 +10,24 @@ namespace Cloud_API.Models {
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             //base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Item>().HasKey(k => k.Key);
+            modelBuilder.Entity<Item>().HasKey(k => k.ID);
         }
 
         public DbSet<Item> Items { get; set; }
     }
 
     public class Item {
-        public int Key { get; set; }
-        public string Name { get; set; }
+        public int ID { get; set; }
+        public DateTime Data { get; set; }
+        public string Equip { get; set; }
+        public double Value { get; set; }
+        public string Aux { get; set; }
+
+        public void Update(Item nouItem) {
+            Data = DateTime.Now;
+            Equip = nouItem.Equip;
+            Value = nouItem.Value;
+            Aux = nouItem.Aux;
+        }
     }
 }
