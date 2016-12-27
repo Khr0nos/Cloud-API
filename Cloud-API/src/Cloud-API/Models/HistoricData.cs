59e07564998afc113b3c7be991fbc40f11b9f310
Xavi Garcia <antiwindows17@gmail.com>
2016-12-27 12:19:32 +0100

    xml comments

--- Cloud-API/src/Cloud-API/Models/HistoricData.cs
+++ Cloud-API/src/Cloud-API/Models/HistoricData.cs
@@ -5,18 +5,24 @@ using System.ComponentModel.DataAnnotations;
 namespace Cloud_API.Models {
     public partial class HistoricData {
         public int IdhistoricData { get; set; }
+        /// <value>1</value>
         [Required(ErrorMessage = "Device id missing")]
         public int Iddevice { get; set; }
-        [DataType(DataType.DateTime)]
-        public DateTime HistDataDate { get; set; }
+        /// <value>null</value>
+        public DateTime? HistDataDate { get; set; }
+        /// <value>123</value>
         [Required(ErrorMessage = "Data Value missing")]
         public string HistDataValue { get; set; }
+        /// <value>1</value>
         [Required(ErrorMessage = "Data type id missing")]
         public int IddataType { get; set; }
+        /// <value>false</value>
         [DefaultValue(false)]
         public bool HistDataToDevice { get; set; } = false;
+        /// <value>false</value>
         [DefaultValue(false)]
         public bool HistDataAck { get; set; } = false;
+        /// <value>null</value>
         [DefaultValue(null)]
         public string HistDataAux { get; set; } = null;
 
