f1e0384c5dd335e9a44ded9c08d0408e4b0361ea
Xavi Garcia <antiwindows17@gmail.com>
2017-01-19 13:21:38 +0100

    provar webjob a consultar BBDD

---
+++ Cloud-API/Cloud-Background-SDK/Functions.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace Cloud_Background {
+    public class Functions {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("DeviceQueue")] string message, TextWriter log) {
+            log.WriteLine(message);
+        }
+    }
+}
---
+++ Cloud-API/Cloud-Background/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace Cloud_Background {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                Console.WriteLine($"test data now: {DateTime.Now}");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
